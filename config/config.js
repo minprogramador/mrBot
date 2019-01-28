744273813d7e443da0a66cca75555f71654f9d59
minprogramador <minprogramador@gmail.com>
2019-01-28 18:14:58 -0200

    add url file config

--- config/config.js
+++ config/config.js
@@ -6,5 +6,5 @@ const config = {};
 config.token = '714388705:AAH8z02IcJrwAdWZNN8GPvE6gfG7-XU03Qo';
 config.url = "191.96.139.176:7544";
 config.filerun = "/Users/bruno/Desktop/public/demo/main.php";
-
+//config.filerun = "/var/www/html/demonew/main.php";
 module.exports = config;
